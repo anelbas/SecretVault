6485be165ffa89ff03ca6b969af5f7ccb797957e
Anel Basson <anelb@bbd.co.za>
2022-07-07 20:53:58 +0200

    Update PostGroup and Post Controllers

--- SecretVaultAPI/Controllers/PostGroupController.cs
+++ SecretVaultAPI/Controllers/PostGroupController.cs
@@ -27,11 +27,18 @@ namespace SecretVaultAPI.Controllers
                 return BadRequest();
             }
 
-            GroupUser newGroupPost = new GroupUser();
+            PostGroup newGroupPost = new PostGroup();
 
             newGroupPost.GroupId = (int)request._groupId;
             newGroupPost.PostId = (int)request._postId;
 
+            bool isPrivate = checkIfPostIsPrivate(newGroupPost.PostId);
+
+            if (isPrivate)
+            {
+                return BadRequest();
+            }
+            
             try
             {
                 _context.PostGroups.Add(newGroupPost);
@@ -44,6 +51,46 @@ namespace SecretVaultAPI.Controllers
 
 
             return Ok(newGroupPost);
+        }
+
+        [HttpDelete("/groupPost/{id}")]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            PostGroup groupPostToDelete = _context.PostGroups.Find(id);
+
+            try
+            {
+                _context.PostGroups.Remove(groupPostToDelete);
+                _context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+
+            return Ok(groupPostToDelete);
+        }
+
+        private bool checkIfPostIsPrivate(int postId)
+        {
+            int privacyStatus = 0;
+            //get privacy status from post
+
+            if(privacyStatus == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
 
     }
