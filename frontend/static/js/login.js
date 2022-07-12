c1771f149f68f70c0a3eabc731593ddba54bfcbe
Jordanvz <jordan@bbd.co.za>
2022-07-12 22:43:25 +0200

    login working with aws

--- frontend/static/js/login.js
+++ frontend/static/js/login.js
@@ -1,9 +1,11 @@
+
+
 //TODO: Call the endpoint with the validated information
 function authenticateLogin(){
 
-    const username=document.getElementById("username").value;
+    const email=document.getElementById("email").value;
     const password=document.getElementById("password").value;
 
-
+    completedSignIn.completedSignIn(email,password);
 
 };
